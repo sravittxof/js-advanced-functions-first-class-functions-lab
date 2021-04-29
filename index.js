5690bd9a6995e8771014a49fcc7a7fec162cf6bd
sravittxof <travis.fox90@gmail.come>
2021-04-28 22:35:10 -0400

    commit tree 1f148b12621479696212a6e9f260e444901c8faf

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function(drivers){
+    const firstTwo = [drivers[0], drivers[1]];
+    return firstTwo;
+};
+
+const returnLastTwoDrivers = function(drivers){
+    const lastTwo = [drivers[(drivers.length - 2)], drivers[(drivers.length - 1)]];
+    return lastTwo;
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(fareMultiplier){
+    return function(fare){
+        return fare * fareMultiplier;
+    };
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers(drivers, returnDrivers){
+    return returnDrivers(drivers);
+}
+
